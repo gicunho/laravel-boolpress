f30d3a2bcd78a9cb68f31e851d6cb14ffefceeab
gicunho <gicunho@hotmail.it>
2021-07-28 17:32:31 +0200

    Creato Modello, Controller e Seeder per Categorie

---
+++ database/seeds/CategorySeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Category;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = ['Coding', 'Automation', "Web design", "Best Practice"];
+
+        foreach ($categories as $category) {
+            $cat = new Category();
+            $cat->name = $category;
+            $cat->slug = Str::slug($cat->name);
+            $cat->save();
+        }
+    }
+}
